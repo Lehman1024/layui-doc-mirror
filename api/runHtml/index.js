68fba2230f3046099d30347f1f84c81a4a509e7d
Lehman <280668200@qq.com>
2023-05-14 13:11:35 +0800

    修正兼容配置

--- api/runHtml/index.js
+++ api/runHtml/index.js
@@ -1,6 +1,13 @@
 module.exports = (req, res) => {
     if (req.method === 'POST') {
-        res.send(decodeURIComponent(req.body.html));
+        let body = '';
+        req.on('data', chunk => {
+            body += chunk.toString();
+        });
+        req.on('end', () => {
+            const parsedBody = JSON.parse(body);
+            res.end(decodeURIComponent(parsedBody.html));
+        });
     } else {
         res.status(405).send("Method Not Allowed");
     }
